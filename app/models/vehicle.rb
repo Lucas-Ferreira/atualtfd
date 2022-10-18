e9026a4fcd6d962dce64b05765ca27dfad5a7206
Lucas <lucas.redes.ferreira@gmail.com>
2022-10-18 15:45:58 -0300

    Fix Dependent Destroy

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,8 +1,8 @@
 class Vehicle < ApplicationRecord
   #belongs_to :user, optional: true
-  has_many :route_vehicles
-  has_many :routes, through: :route_vehicles
-  has_many :travel_vehicles
+  has_many :route_vehicles, dependent: :delete_all
+  has_many :routes, through: :route_vehicles, dependent: :delete_all
+  has_many :travel_vehicles, dependent: :delete_all
   has_many :travel, through: :travel_vehicles
   validates :modelo, presence: true
   validates :ano, presence: true

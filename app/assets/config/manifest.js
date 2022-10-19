d1736eccbbe58889b83f4dc367954ce6703d3c1a
Lucas <lucas.redes.ferreira@gmail.com>
2022-10-19 13:36:04 -0300

    Fix all

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,9 +1,4 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../builds
-//= link jquery.js
-//= link jquery_ujs
-//= link app.js
-//= link bootstrap.bundle.min
-//= link scripts.js
 //= require turbolinks

310873a68c2812c8e67ae475d5d38a803f4c60b0
Lucas <lucas.redes.ferreira@gmail.com>
2022-10-19 12:28:40 -0300

    Resolve Turbolink F

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,9 +4,3 @@
 //= require turbolinks
 //= require_tree .
 import "@hotwired/turbo-rails"
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()

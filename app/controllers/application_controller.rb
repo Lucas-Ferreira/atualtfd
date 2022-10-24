5ead2b93f5358d1d65852dac587ebcd3705a245a
Lucas <lucas.redes.ferreira@gmail.com>
2022-10-24 14:39:06 -0300

    Fix Devise User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,6 @@ class ApplicationController < ActionController::Base
   #end
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
-  def after_sign_in_path_for(resource)
-    user_profile_path(current_user)
-  end
-
   # Uncomment when you *really understand* Pundit!
   # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   # def user_not_authorized

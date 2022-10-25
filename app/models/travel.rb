505e47e8040e1f4c5de127b404af2270c2b6e93a
Lucas <lucas.redes.ferreira@gmail.com>
2022-10-25 15:25:41 -0300

    Fix Travel Dependent:Destroy

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -2,6 +2,6 @@ class Travel < ApplicationRecord
   belongs_to :route
   has_many :travel_vehicles, dependent: :destroy
   has_many :vehicles, through: :travel_vehicles
-  has_many :confirmations
+  has_many :confirmations, dependent: :destroy
   has_many :patients, through: :confirmations
 end
